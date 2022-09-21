a271a934e459c2ef6c23ea9e23acb24ad2edc598
João Matheus Silva Franca <matheussfranca@live.com>
2022-09-21 15:02:25 -0300

    Iniciando o Header do portfólio

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Portfólio de João Matheus</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
