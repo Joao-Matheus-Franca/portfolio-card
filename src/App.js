ef9fab4c821ea6f48528a0bf073872ca0bf774cb
João Matheus Silva Franca <matheussfranca@live.com>
2022-09-21 15:48:59 -0300

    Adicionando os primeiros dados

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Components/Header';
+import './App.css'
 
 class App extends React.Component {
   render() {
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,12 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return(
-      <div>
-        <h1>Portfólio de João Matheus</h1>
+      <div id="main-header">
+        <h1>Programmer Card</h1>
+        <h2>Nome:<span class='values'> João Matheus Silva Franca </span></h2>
+        <h3>Data de nascimento:<span class='values'> 20/11/1996 </span></h3>
+        <h3>Local de nascimento:<span class='values'> Juazeiro - Bahia - Brasil </span></h3>
+        <h3>Pronomes:<span class='values'> Ele/Dele </span></h3>
       </div>
     );
   }
